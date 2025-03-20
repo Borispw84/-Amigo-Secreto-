9dc13d05418bda84162583689485c9ee764d9e64
Borispw84 <bgonzalezmolinet@gmail.com>
2025-03-20 08:17:57 -0500

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-<motion.img
-  src="/images/pst.jpg"
-  alt="PST"
-  style={{ width: "50px", height: "50px" }}  // 50px para reducir el tamaño
-  className="cursor-pointer"
-  onClick={() => handleViewChange("pst")}
-  whileHover={{ scale: 1.1 }}
-/>
