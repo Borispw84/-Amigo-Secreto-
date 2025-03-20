d97ee673ca912fd7197272d7180fcb65f291d2e4
Borispw84 <bgonzalezmolinet@gmail.com>
2025-03-20 08:05:01 -0500

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children }) {
-  return (
-    <div className="p-4 bg-white shadow-md rounded-lg w-80 text-center">
-      {children}
-    </div>
-  );
-}
